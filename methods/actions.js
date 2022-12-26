0810e5114349b228e3b9c7542cc140e00243490d
onurhan1999 <83463950+onurhan1999@users.noreply.github.com>
2022-12-26 11:37:19 +0300

    add fav function working well

--- methods/actions.js
+++ methods/actions.js
@@ -5,25 +5,36 @@ const Message = require("../models/message");
 var jwt = require('jwt-simple')
 var config = require('../config/dbconfig')
 const { authenticate } = require('passport')
+var List = require("collections/list");
+
+
+var ObjectId = require('mongodb').ObjectId;
+
+
 
 var mongoose = require('mongoose');
 
 
 var functions = {
+
     addNew: function (req, res) {
         User.findOne({
             mail: req.body.mail
         }, function (err, user) {
+            var array = [];
+
             if (err) {
                 throw err
             } else {
                 if ((!req.body.name) || (!req.body.password) || (!req.body.mail)) {
                     res.json({ succes: false, msg: 'Boş alanları doldurun.' })
                 } else {
+
                     var newUser = User({
                         name: req.body.name,
                         password: req.body.password,
-                        mail: req.body.mail
+                        mail: req.body.mail,
+                        favList: array,
                     });
                     if (user) {
                         res.status(403).send({ succes: false, msg: 'Kayıt başarısız. Bu mail zaten kayıtlı.' })
@@ -80,20 +91,40 @@ var functions = {
 
         )
     },
-    getinfo: function (req, res) {
-        if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-            var token = req.headers.authorization.split(' ')[1]
-            var decodedtoken = jwt.decode(token, config.secret)
-            return res.json({ succes: true, msg: 'Hello' + decodedtoken.name })
 
-        }
-        else {
-            return res.json({ succes: false, msg: 'No Headers' })
 
+    getUserInfo: function (req, res) {
+        console.log(req.query.mail)
+
+        User.findOne({
+            mail: req.query.mail
+        }, function (err, user) {
+            if (err) {
+                console.log("error")
+                throw err
+            }
+            if (user) {
+                console.log("user")
+                console.log(user.favList)
+                res.status(200).json({
+                    name: user.name,
+                    mail: user.mail,
+                    favList: user.favList,
+                    id: user.id
+                })
+
+            }
+            else {
+                res.status(403).send({ succes: false, msg: 'İşlem Başarısız.' })
+            }
         }
+
+        )
     },
+
+
     addAdvert: function (req, res) {
-        if ((!req.body.title) || (!req.body.description) || (!req.body.price) || (!req.body.province) || (!req.body.district) || (!req.body.numberOfRooms) || (!req.body.street)||(!req.body.isFavourite)) {
+        if ((!req.body.title) || (!req.body.description) || (!req.body.price) || (!req.body.province) || (!req.body.district) || (!req.body.numberOfRooms) || (!req.body.street) || (!req.body.isFavourite)) {
 
             res.json({ succes: false, msg: 'Enter all fields' })
         }
@@ -106,7 +137,7 @@ var functions = {
                 district: req.body.district,
                 street: req.body.street,
                 numberOfRooms: req.body.numberOfRooms,
-                isFavourite:req.body.isFavourite
+                isFavourite: req.body.isFavourite
 
             });
             advert.save(function (err, advert) {
@@ -145,28 +176,69 @@ var functions = {
     },
 
     updateAdvertsFavField: function (req, res) {
-        var favBool;
+        console.log("updateAdvertsFavField")
+        connection = mongoose.connection;
+        connection.collection("users").find({ $and: [ { favList: req.body.advertId }, { _id: ObjectId(req.body.userId) } ] }).toArray(function (err, info) {
 
-        if(req.body.isFavourite=="true"){
-            favBool=true;
-        }else{
-            favBool=false;
-
-        }
-
-
-        Advert.updateOne({ _id: req.body._id }, { $set: {isFavourite: favBool}},{upsert:false} ,function (err) {
-            if (!err) {
+            if (err) {
                 res.json({
-                    succes: true, msg: 'Favori güncellemesi başarılı.'
+                    succes: false,
+                    msg: 'updateAdvertsFavField fonksiyonu başarısız oldu.',
                 })
+            } else {
+                console.log("else içi")
+                console.log(info)
+                if (info.length == 0) {
+                    // değer listede olmadığı için değeri listeye ekleyeceğiz burada
+                    connection.collection("users").findOneAndUpdate({ _id: ObjectId(req.body.userId), }, { $push: { favList: req.body.advertId } }, { upsert: false }, function (err) {
+                        console.log("findoneandupdate için")
+
+                        if (!err) {
+                            res.json({
+                                succes: true, msg: 'Favori eklemesi başarılı.'
+                            })
+                        }
+                        else {
+                            res.json({
+                                succes: false, msg: 'Favori eklemesi başarısız.'
+                            })
+                        }
+                    });
+
+
+                } else {
+
+                    connection.collection("users").findOneAndUpdate({ _id: ObjectId(req.body.userId), }, { $pull: { favList: req.body.advertId } }, function (err) {
+                        console.log("findoneandupdate için")
+
+                        if (!err) {
+                            res.json({
+                                succes: true, msg: 'Favori silme başarılı.'
+                            })
+                        }
+                        else {
+                            res.json({
+                                succes: false, msg: 'Favori silme başarısız.'
+                            })
+                        }
+                    });
+
+
+
+                    //değer listede var olduğu için değeri listeden çıkaracağız
+                }
+
+
             }
-            else {
-                res.json({
-                    succes: false, msg: 'Favori güncellemesi başarısız.'
-                })
-            }
-        });
+        })
+
+
+
+
+
+
+
+
 
     },
 
@@ -198,7 +270,7 @@ var functions = {
                 from: req.body.from,
                 to: req.body.to,
                 message: req.body.message,
-                sendAt: req.body.sendAt ,
+                sendAt: req.body.sendAt,
             });
             message.save(function (err, message) {
                 if (err) {
@@ -216,9 +288,9 @@ var functions = {
         }
     },
 
+
     getMessages: function (req, res) {
         connection = mongoose.connection;
-
         connection.collection("messages").find({
         }).toArray(function (err, info) {
             if (err) {
@@ -232,10 +304,8 @@ var functions = {
             }
         })
     },
-
     getMessageByUser: function (req, res) {
         connection = mongoose.connection;
-
         connection.collection("adverts").find({
         }).toArray(function (err, info) {
             if (err) {
@@ -258,27 +328,121 @@ var functions = {
 
 
 
+    addFavAdvert: function (req, res) {
+
+        var list = new List([]);
+        var array = [];
+        array.push("senden");
+        console.log(array[0])
+        connection = mongoose.connection;
+        User.updateOne({ _id: req.body.userId }, { $push: { favList: req.body.advertId } }, { upsert: false }, function (err) {
+
+
+
+            if (!err) {
+                res.json({
+                    succes: true, msg: 'Favori eklemesi başarılı.'
+                })
+            }
+            else {
+                res.json({
+                    succes: false, msg: 'Favori eklemesi başarısız.'
+                })
+            }
+        });
+    },
+
+
+    getFavAdverts: function (req, res) {
+        connection = mongoose.connection;
+        //var array=req.body.list
+
+        console.log("getFavAdverts")
+        var array = []
+        var idArray = [];
+
+        array = req.query.list;
+
+        if (typeof array == "string") {
+            idArray.push(ObjectId(array))
+
+
+          connection.collection("adverts").find({ _id: { "$in": idArray }, }).toArray(function (err, info) {
+
+                if (err) {
+                    res.json({
+                        success: false,
+                        msg: 'getDataWithFilter1 fonksiyonu başarısız oldu.',
+                    })
+                } else {
+                    console.log(info)
+                    res.status(200).json({
+                        success:true,
+                        data: info
+                    })
+                }
+            })
+
+        } else if (typeof array == "undefined") {
+
+            res.json({
+                success: false,
+                msg: 'Liste boş.',
+            })
+
+        } else {
+
+
+            array.forEach(element => {
+                idArray.push(ObjectId(element))
+            });
+
+
+
+
+            connection.collection("adverts").find({ _id: { "$in": idArray }, }).toArray(function (err, info) {
+
+                if (err) {
+                    res.json({
+                        success: false,
+                        msg: 'getDataWithFilter1 fonksiyonu başarısız oldu.',
+                    })
+                } else {
+                    console.log(info)
+                    res.status(200).json({
+                        success:true,
+                        data: info
+                    })
+                }
+            })
+    
+
+        }
+
+
+
+
+
+
+        //bir string listesi göndereceğim ve bu listeyi foreach ile dönüp her elemanını objarraye ekleyeceğiz ve bunu da aşağıdaki gibi aratacağız
+       
+
+    },
+
+
+
+
     getDataWithFilter: function (req, res) {
         console.log("getdatawithfilter")
-    
-        console.log(req.query.province)
-        console.log(req.query.district)
-        console.log(req.query.numberOfRooms)
-
         connection = mongoose.connection;
-
         if (req.query.province && req.query.district && req.query.numberOfRooms) {
             console.log("HEM İL HEM İLÇE HEM DE ODA SAYISI VARSA")
 
             //HEM İL HEM İLÇE HEM DE ODA SAYISI VARSA
             connection.collection("adverts").find({
-
                 province: req.query.province,
                 district: req.query.district,
                 numberOfRooms: req.query.numberOfRooms,
-              
-
-
             }).toArray(function (err, info) {
                 if (err) {
                     res.json({
@@ -292,137 +456,103 @@ var functions = {
                 }
             })
         } else if (req.query.province && req.query.district && !req.query.numberOfRooms) {
-            
             console.log("SADECE İL VE İLÇE VARSA ODA YOKSA")
             console.log(req.query.province)
             console.log(req.query.district)
             //SADECE İL VE İLÇE VARSA ODA YOKSA
             connection.collection("adverts").find({
-
                 province: req.query.province,
                 district: req.query.district,
-                
             }).toArray(function (err, info) {
-                if(err){
+                if (err) {
                     res.json({
                         succes: false,
-                         msg: 'getDataWithFilter1 fonksiyonu başarısız oldu.',
-                         
-    
+                        msg: 'getDataWithFilter1 fonksiyonu başarısız oldu.',
                     })
-                }else{
+                } else {
                     res.status(200).json({
-                        data:info
+                        data: info
                     })
                 }
-              })
-
+            })
         } else if (req.query.province && !req.query.district && !req.query.numberOfRooms) {
             console.log("SADECE İL VARSA İLÇE VE ODA YOKSA")
-
             //SADECE İL VARSA İLÇE VE ODA YOKSA
             connection.collection("adverts").find({
-        
                 province: req.query.province,
-                
-              
-                
-    
             }).toArray(function (err, info) {
-                if(err){
+                if (err) {
                     res.json({
                         succes: false,
-                         msg: 'getDataWithFilter1 fonksiyonu başarısız oldu.',
-                         
-    
+                        msg: 'getDataWithFilter1 fonksiyonu başarısız oldu.',
                     })
-                }else{
+                } else {
                     res.status(200).json({
-                        data:info
+                        data: info
                     })
                 }
-              })
+            })
         }
         else if (req.query.province && !req.query.district && req.query.numberOfRooms) {
             console.log("SADECE İL VARSA VE ODA VARSA")
             //SADECE İL VARSA VE ODA VARSA
             connection.collection("adverts").find({
-        
                 province: req.query.province,
                 numberOfRooms: req.query.numberOfRooms,
-              
-                
-    
             }).toArray(function (err, info) {
-                if(err){
+                if (err) {
                     res.json({
                         succes: false,
-                         msg: 'getDataWithFilter1 fonksiyonu başarısız oldu.',
-                         
-    
+                        msg: 'getDataWithFilter1 fonksiyonu başarısız oldu.',
                     })
-                }else{
+                } else {
                     res.status(200).json({
-                        data:info
+                        data: info
                     })
                 }
-              })
+            })
         }
         else if (!req.query.province && !req.query.district && req.query.numberOfRooms) {
             console.log("İL VE İLçE YOK SADECE ODA VAR")
 
             //İL VE İLçE YOK SADECE ODA VAR
             connection.collection("adverts").find({
-        
                 numberOfRooms: req.query.numberOfRooms,
-                
-              
-                
-    
             }).toArray(function (err, info) {
-                if(err){
+                if (err) {
                     res.json({
                         succes: false,
-                         msg: 'getDataWithFilter1 fonksiyonu başarısız oldu.',
-                         
-    
+                        msg: 'getDataWithFilter1 fonksiyonu başarısız oldu.',
+
+
                     })
-                }else{
+                } else {
                     res.status(200).json({
-                        data:info
+                        data: info
                     })
                 }
-              })
+            })
 
         } else {
             console.log("HİÇBİRİ YOKSA BÜTÜN DATALARI GÖNDERİR")
 
             //HİÇBİRİ YOKSA BÜTÜN DATALARI GÖNDERİR
             connection.collection("adverts").find({}).toArray(function (err, info) {
-                if(err){
+                if (err) {
                     res.json({
                         succes: false,
-                         msg: 'getDataWithFilter1 fonksiyonu başarısız oldu.',
-                         
-    
+                        msg: 'getDataWithFilter1 fonksiyonu başarısız oldu.',
+
+
                     })
-                }else{
+                } else {
                     res.status(200).json({
-                        data:info
+                        data: info
                     })
                 }
-              })
+            })
         }
-
-
-
-
-
     },
-
-
-
-
 
 }
 
