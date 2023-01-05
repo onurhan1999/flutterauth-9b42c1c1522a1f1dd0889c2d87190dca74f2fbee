656fd0091a07089bf0de6c0a5c803885dfbde32a
onurhan1999 <83463950+onurhan1999@users.noreply.github.com>
2023-01-05 23:52:35 +0300

    add admin panel

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const actions= require('../methods/actions')
 const router = express.Router()
 
+
+
+
+
 router.get('/', (req, res) => {
     res.send('Hello World')
 })
@@ -10,31 +14,29 @@ router.get('/dashboard', (req, res) => {
     res.send('Dashboard')
 })
 
-
-router.post('/authenticate',actions.authenticate)
+//Admin işlemlerinin yapıldığı metodlar
 router.post('/adminAuthenticate',actions.adminAuthenticate)
 router.post('/adminAddNew',actions.adminAddNew)
 
-
-router.post('/adduser',actions.addNew)
-router.post('/addAdvert',actions.addAdvert)
-
-router.post('/deleteAdvert',actions.deleteAdvert)
-router.post('/updateAdvertsFavField',actions.updateAdvertsFavField)
-
+//Kullanıcı işlemlerinin yapıldığı metodlar
+router.post('/adduser',actions.addUser)
+router.post('/authenticate',actions.authenticate)
 router.get('/getUserInfo',actions.getUserInfo)
 
-
+//İlan işlemlerinin yapıldığı metodlar
+router.post('/addAdvert',actions.addAdvert)
+router.post('/deleteAdvert',actions.deleteAdvert)
 router.get('/getData',actions.getData)
 router.get('/getDataWithFilter',actions.getDataWithFilter)
-router.post('/addFavAdvert',actions.addFavAdvert)
-
+router.post('/updateAdvertsFavField',actions.updateAdvertsFavField)
 router.get('/getFavAdverts',actions.getFavAdverts)
+
 
 
 router.get('/sendMessage',actions.sendMessage)
 router.get('/getMessages',actions.getMessages)
 router.get('/getMessageByUser',actions.getMessageByUser)
+
 
 
 module.exports = router
